cf89ad35eeb5bd3f09ed77c8c31670829ebbc409
lanyablue <barbaramy_@hotmail.com>
2022-08-19 14:49:01 -0300

    modelagem dominio

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,20 +1,23 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
 
     private inputData: HTMLInputElement;  // tipagem html input 
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
 
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor')
+        this.inputValor = document.querySelector('#valor');
     }
 
     adiciona(): void {
        const negociacao = this.criaNegociacao();
-       console.log(negociacao)
+       this.negociacoes.adiciona(negociacao);
+       console.log(this.negociacoes.lista());
        this.limparFormulario();
     }
 
